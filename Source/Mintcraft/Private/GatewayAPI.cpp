120a2da38c6eb5ec67c8adca8c1409bfaa62d75e
Tang Bo Hao <tech@btang.cn>
2021-07-16 21:49:20 +0800

    fix error

--- Source/Mintcraft/Private/GatewayAPI.cpp
+++ Source/Mintcraft/Private/GatewayAPI.cpp
@@ -28,20 +28,20 @@ namespace mintcraft
   {
 
     //Create a pointer to hold the json serialized data
-    TSharedPtr JsonObject;
+    // TSharedPtr JsonObject;
 
-    //Create a reader pointer to read the json data
-    TSharedRef Reader = TJsonReaderFactory::Create(Response->GetContentAsString());
+    // //Create a reader pointer to read the json data
+    // TSharedRef Reader = TJsonReaderFactory::Create(Response->GetContentAsString());
 
-    //Deserialize the json data given Reader and the actual object to deserialize
-    if (FJsonSerializer::Deserialize(Reader, JsonObject))
-    {
-      // TODO handle json response
-      // //Get the value of the json object by field name
-      // int32 recievedInt = JsonObject->GetIntegerField("customInt");
+    // //Deserialize the json data given Reader and the actual object to deserialize
+    // if (FJsonSerializer::Deserialize(Reader, JsonObject))
+    // {
+    //   // TODO handle json response
+    //   // //Get the value of the json object by field name
+    //   // int32 recievedInt = JsonObject->GetIntegerField("customInt");
 
-      // //Output it to the engine
-      // GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, FString::FromInt(recievedInt));
-    }
+    //   // //Output it to the engine
+    //   // GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, FString::FromInt(recievedInt));
+    // }
   }
 }
