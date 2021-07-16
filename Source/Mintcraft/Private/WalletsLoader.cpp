b374241ce70e736e5b001676562d058974b05a06
Tang Bo Hao <tech@btang.cn>
2021-07-16 10:56:33 +0800

    FIX COMPILE

--- Source/Mintcraft/Private/WalletsLoader.cpp
+++ Source/Mintcraft/Private/WalletsLoader.cpp
@@ -6,6 +6,34 @@
 
 namespace mintcraft
 {
+  static FString PathNormalize(const FString& PathIn)
+  {
+    TArray<FString> PathFrags;
+    PathIn.ParseIntoArray(PathFrags, TEXT("/"));
+    Algo::Reverse(PathFrags);
+    TArray<FString> NewPathFrags;
+    bool FromRoot = PathIn.StartsWith(TEXT("/"));
+    while (PathFrags.Num() > 0) {
+      FString E = PathFrags.Pop();
+      if (E != TEXT("") && E != TEXT("."))
+      {
+        if (E == TEXT("..") && NewPathFrags.Num() > 0 && NewPathFrags.Last() != TEXT("..")) {
+          NewPathFrags.Pop();
+        }
+        else {
+          NewPathFrags.Push(E);
+        }
+      }
+    }
+    if (FromRoot)
+    {
+      return TEXT("/") + FString::Join(NewPathFrags, TEXT("/"));
+    }
+    else
+    {
+      return FString::Join(NewPathFrags, TEXT("/"));
+    }
+  }
 
   bool WalletLoader::LoadNames(TArray<FString> &Names)
   {
@@ -22,7 +50,7 @@ namespace mintcraft
   bool WalletLoader::CheckExists(const FString &PathIn, FString &Path, FString &AbsolutePath)
   {
     IPlatformFile &PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
-    FString NormalizedPath = puerts::PathNormalize(PathIn);
+    FString NormalizedPath = PathNormalize(PathIn);
     if (PlatformFile.FileExists(*NormalizedPath))
     {
       AbsolutePath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*NormalizedPath);
